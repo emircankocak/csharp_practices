b363ed8d3616e23f4bf1b5e4bb5ff6d5cac4ae69
emircankocak <emircankocak777@gmail.com>
2026-04-16 11:16:20 +0300

    Arrays-1 eklendi

---
+++ Arrays-1/Arrays-1/Program.cs
@@ -0,0 +1,18 @@
+﻿int[] numbers = new int[7]; // 7 elemanlık bir dizi boyutu oluşturuyoruz.
+for (int i = 0; i < numbers.Length; i++)
+{
+    Console.Write("Enter a number:\t"); // Kullanıcının diziye eleman aktarması. 
+    if (int.TryParse(Console.ReadLine(), out int value))
+    {
+        numbers[i] = value;
+    }
+    else
+    {
+        Console.WriteLine("Invalid number");// Kullanıcı eğer integer değer aralığın dışında bir eleman girerse geçersiz sayılır.
+    }
+}
+foreach (int value in numbers)
+{
+    Console.WriteLine($"\n Your number is {value}");
+}
+
